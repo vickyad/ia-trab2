aa4e1bc788ef94976d5c71393953b640541158f6
Victoria <victoria.ad40@gmail.com>
2021-09-02 17:14:12 -0300

    maybe something works

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import copy
+
 import your_agent.agent as nha
 import board
 
 if __name__ == '__main__':
     initial_board = board.Board()
-    print(nha.minimax_alphabeta({'board': initial_board.tiles, 'player': 'B'}, initial_board, 'B', 4))
+    print(nha.make_move(initial_board, 'B'))
--- your_agent/agent.py
+++ your_agent/agent.py
@@ -1,34 +1,70 @@
+import copy
 import board
-import random
 import sys
-
 
 # Voce pode criar funcoes auxiliares neste arquivo
 # e tambem modulos auxiliares neste pacote.
 #
 # Nao esqueca de renomear 'your_agent' com o nome
 # do seu agente.
+
+
 def minimax_alphabeta(original_state, current_board: board.Board, color: str, depth: int, is_max_turn=True):
+    global move
+    print('Entrei na função minimax')
+    print('Board atual:')
+    current_board.print_board()
+
     if current_board.is_terminal_state() or depth == 0:
+        print('Hora da decisão')
+        print(f'Depth: {depth} // Estado terminal? {current_board.is_terminal_state()}')
         return pieces_diff(original_state['board'], current_board.tiles, original_state['player'])
     successors = current_board.legal_moves(color)
+    print(f'Sucessores {successors}')
     if is_max_turn:
+        print('Vez do MAX')
         v = - sys.maxunicode
         for s in successors:
+            print(f'Avaliando: {s}')
             current_board.process_move(s, color)
+            current_board.print_board()
             new_v = minimax_alphabeta(original_state, current_board, current_board.opponent(color), depth - 1, False)
-            v = max(v, new_v)
+            print(f'New_v: {new_v} do {s}')
+            if new_v > v:
+                v = new_v
+                move = s
+            print(f'V: {v} do {s}')
         return v
     else:
+        print('Vez do MIN')
         v = sys.maxunicode
         for s in successors:
+            print(f'Avaliando: {s}')
             current_board.process_move(s, color)
+            current_board.print_board()
             new_v = minimax_alphabeta(original_state, current_board, current_board.opponent(color), depth - 1, True)
-            v = min(v, new_v)
+            print(f'New_v: {new_v} do {s}')
+
+            if new_v < v:
+                v = new_v
+                move = s
+            print(f'V: {v} do {s}')
+        return v
 
 
 def pieces_diff(original_board_tiles, current_board_tiles, color):
-    return original_board_tiles.count(color) - current_board_tiles.count(color)
+    print('Fazendo os calculos com')
+    print(original_board_tiles)
+    print(current_board_tiles)
+    ob_tile_count = 0
+    for line in original_board_tiles:
+        ob_tile_count += line.count(color)
+    cb_tile_count = 0
+    for line in current_board_tiles:
+        cb_tile_count += line.count(color)
+    print(f'Quantidade de {color} no original board: {ob_tile_count}')
+    print(f'Quantidade de {color} na table atual: {cb_tile_count}')
+    return ob_tile_count - cb_tile_count
 
 
 def make_move(the_board, color):
@@ -41,5 +77,5 @@ def make_move(the_board, color):
     # o codigo abaixo apenas retorna um movimento aleatorio valido para
     # a primeira jogada com as pretas.
     # Remova-o e coloque a sua implementacao da poda alpha-beta
-    return random.choice([(2, 3), (4, 5), (5, 4), (3, 2)])
-
+    minimax_alphabeta({'board': copy.deepcopy(the_board.tiles), 'player': color}, the_board, color, 4)
+    return move
